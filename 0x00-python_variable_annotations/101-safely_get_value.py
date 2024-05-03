606d94590998b85f3742155625aac5b6076cafa8
Mohamed Osama <mossama370@gmail.com>
2024-05-03 03:45:54 +0300

    Update 101-safely_get_value.py

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -9,7 +9,7 @@ def safely_get_value(
   dct: Mapping, key: Any, default: Union[T, None] = None
 ) -> Union[Any, T]:
   """safely_get_value"""
-  if key in dct:
+if key in dct:
   return dct[key]
-  else:
+else:
   return default
