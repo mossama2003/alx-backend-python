b0ff756331314f2918a80bf470458218ba80dc96
Mohamed Osama <mossama370@gmail.com>
2024-05-03 03:21:21 +0300

    Update 1-concat.py

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -7,11 +7,11 @@ as arguments and returns a concatenated string"""
 def concat(str1: str, str2: str) -> str:
 	"""_summary_
 
-Args:
+    Args:
 str1 (string): string
 str2 (string): string
 
-Returns:
+    Returns:
 string: a concatenated string
 """
 	return str1 + str2
