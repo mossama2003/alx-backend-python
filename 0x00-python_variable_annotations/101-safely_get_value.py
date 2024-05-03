ccfb1802e56238671d56e2c9db91fe7697a73e87
Mohamed Osama <mossama370@gmail.com>
2024-05-03 03:47:05 +0300

    Update 101-safely_get_value.py

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -4,7 +4,6 @@ from typing import Sequence, Any, Union, Mapping, TypeVar
 
 T = TypeVar("T", bound=Any)
 
-
 def safely_get_value(
   dct: Mapping, key: Any, default: Union[T, None] = None) -> Union[Any, T]:
   """safely_get_value"""
