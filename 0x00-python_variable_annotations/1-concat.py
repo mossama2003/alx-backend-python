37a4b94494aadb3cf1cdf89aa1d13197db71becc
Mohamed Osama <mossama370@gmail.com>
2024-05-03 03:30:40 +0300

    Update 1-concat.py

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""Write a type-annotated function concat
+"""a script a type-annotated function concat
 that takes a string str1 and a string str2
 as arguments and returns a concatenated string"""
 
