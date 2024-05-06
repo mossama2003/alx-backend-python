88b7fa8e67b1ade3b203b4fe1983b2a069dd234d
Mohamed Osama <mossama370@gmail.com>
2024-05-06 16:40:17 +0300

    Update 1-concurrent_coroutines.py

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
-    """
-    an asynchronous coroutine
-    that takes in an integer argument
-    (max_delay, with a default value of 10)
+"""
+     an asynchronous coroutine
+     that takes in an integer argument
+     (max_delay, with a default value of 10)
     """
 
 from typing import List
@@ -12,7 +12,7 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
     """return the list of all the delays (float values).
-    in ascending order without using sort() because of concurrency.
+in ascending order without using sort() because of concurrency.
     """
     sorted_tasks = []
     tasks = [wait_random(max_delay) for _ in range(n)]
