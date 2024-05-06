d31f2de98430c0ec6c349229d471fc55abf18d50
Mohamed Osama <mossama370@gmail.com>
2024-05-06 16:43:27 +0300

    Update 1-concurrent_coroutines.py

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,23 +1,28 @@
 #!/usr/bin/env python3
 """
-     an asynchronous coroutine
-     that takes in an integer argument
-     (max_delay, with a default value of 10)
-    """
+    1-concurrent_coroutines.py
+"""
+import asyncio
 
 from typing import List
-import asyncio
-wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+wait_random = __import__("0-basic_async_syntax").wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
-    """return the list of all the delays (float values).
-in ascending order without using sort() because of concurrency.
     """
-    sorted_tasks = []
-    tasks = [wait_random(max_delay) for _ in range(n)]
-    completed = asyncio.as_completed(tasks)
-    for task in completed:
-        result = await task
-        sorted_tasks.append(result)
-    return sorted_tasks # list of delay of tasks sorte as completed
+    wait_n
+    @n: times to call wait_random
+    @max_delay: max delay of wait_random
+    Return: list of all delays
+    """
+
+    delays: List[asyncio.Future[float]] = []
+    for _ in range(n):
+        delays.append(wait_random(max_delay))
+
+    result: List[float] = []
+    for delay in asyncio.as_completed(delays):
+        result.append(await delay)
+    return result
