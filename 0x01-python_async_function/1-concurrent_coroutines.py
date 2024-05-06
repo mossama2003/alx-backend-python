76bb39c0ca31f2c9f7760c2b2cea0daef5a039e2
Mohamed Osama <mossama370@gmail.com>
2024-05-06 16:39:48 +0300

    Update 1-concurrent_coroutines.py

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
-"""
-an asynchronous coroutine
-that takes in an integer argument
-(max_delay, with a default value of 10)
-"""
+    """
+    an asynchronous coroutine
+    that takes in an integer argument
+    (max_delay, with a default value of 10)
+    """
 
 from typing import List
 import asyncio
@@ -11,9 +11,9 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
-"""return the list of all the delays (float values).
-in ascending order without using sort() because of concurrency.
-"""
+    """return the list of all the delays (float values).
+    in ascending order without using sort() because of concurrency.
+    """
     sorted_tasks = []
     tasks = [wait_random(max_delay) for _ in range(n)]
     completed = asyncio.as_completed(tasks)
